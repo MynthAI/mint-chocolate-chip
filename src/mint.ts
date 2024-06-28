93fa259a4a979522aad86f2a40c1458099b3d036
SynthLuvr <131367121+SynthLuvr@users.noreply.github.com>
2024-06-28 22:11:26 +0200

    Created basic cli

--- src/mint.ts
+++ src/mint.ts
@@ -1,1 +1,30 @@
-console.log("hello");
+import { Blockfrost, Wallet } from "@cardano-ts/node";
+import { type } from "arktype";
+import { Command } from "commander";
+
+const program = new Command()
+  .name("mint")
+  .description("Mint a new token")
+  .argument("<address>", "The address of the wallet performing the mint")
+  .action(async (address) => {
+    const config = Config(process.env);
+    if (config instanceof type.errors) return logThenExit(config.summary);
+
+    const wallet = await Wallet.fromAddress(
+      new Blockfrost(config.BLOCKFROST_API_KEY),
+      address
+    );
+
+    if (!wallet.utxos.length) return logThenExit("Wallet must be funded");
+  });
+
+const logThenExit = (message: string): never => {
+  console.error(message);
+  process.exit(1);
+};
+
+const Config = type({
+  BLOCKFROST_API_KEY: "string==39",
+});
+
+program.parseAsync(process.argv);
