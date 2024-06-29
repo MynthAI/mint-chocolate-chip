bda94d9c4720ba10dea3e6dc9b342d7bc58de054
SynthLuvr <131367121+SynthLuvr@users.noreply.github.com>
2024-06-29 08:16:10 +0200

    Created off-chain code to mint token (#1)

---
+++ src/mint.ts
@@ -0,0 +1,144 @@
+import fs from "fs/promises";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { Blockfrost as CardanoBlockfrost, Wallet } from "@cardano-ts/node";
+import {
+  applyParamsToScript,
+  Blockfrost,
+  Data,
+  fromText,
+  Lucid,
+  MintingPolicy,
+  mintingPolicyToId,
+  Network,
+  TxSignBuilder,
+  UTxO,
+} from "@lucid-evolution/lucid";
+import { type, Type } from "arktype";
+import { Command } from "commander";
+
+const here = dirname(fileURLToPath(import.meta.url));
+const expiresIn = 600000; // About 10 minutes
+
+const program = new Command()
+  .name("mint")
+  .description("Mint a new token")
+  .argument("<address>", "The address of the wallet performing the mint")
+  .argument("<name>", "The name of the token to mint")
+  .argument("<amount>", "The amount of token to mint")
+  .action(async ($address, $name, $amount) => {
+    const address = validate(type("string"), $address);
+    const name = validate(TokenName, $name);
+    const amount = validate(Amount, $amount);
+    const config = validate(Config, process.env);
+
+    const projectId = config.BLOCKFROST_API_KEY;
+    const blockfrost = new CardanoBlockfrost(projectId);
+    const wallet = await Wallet.fromAddress(blockfrost, address);
+    if (!wallet.utxos.length) return logThenExit("Wallet must be funded");
+
+    const plutusPath = join(here, "..", "plutus.json");
+    const plutus = Plutus(JSON.parse(await fs.readFile(plutusPath, "utf8")));
+    if (plutus instanceof type.errors) return logThenExit(plutus.summary);
+
+    const txs: TxSignBuilder[] = [];
+    const lucid = await Lucid(
+      new Blockfrost(
+        `https://cardano-${blockfrost.network}.blockfrost.io/api/v0`,
+        projectId
+      ),
+      convertNetwork(blockfrost)
+    );
+    lucid.selectWallet.fromAddress(wallet.address, wallet.utxos);
+
+    const setup = lucid
+      .newTx()
+      .validTo(Date.now() + expiresIn)
+      .pay.ToAddress(wallet.address, { lovelace: 2000000n });
+    const [[ref, ...setupUtxos], , setupTx] = await setup.chain();
+    lucid.selectWallet.fromAddress(wallet.address, setupUtxos);
+    txs.push(setupTx);
+
+    const script = createScript(plutus, ref);
+    const policy = mintingPolicyToId(script);
+    const token = policy + name;
+    const deploy = lucid
+      .newTx()
+      .validTo(Date.now() + expiresIn)
+      .pay.ToAddressWithData(
+        wallet.address,
+        { kind: "inline", value: Data.void() },
+        { lovelace: 4000000n },
+        script
+      );
+    const [[refScript, ...deployUtxos], , deployTx] = await deploy.chain();
+    if (!refScript.scriptRef) return logThenExit("Script didn't deploy");
+    lucid.selectWallet.fromAddress(wallet.address, deployUtxos);
+    txs.push(deployTx);
+
+    const tx = lucid
+      .newTx()
+      .validTo(Date.now() + expiresIn)
+      .mintAssets({ [token]: amount }, Data.void())
+      .readFrom([refScript])
+      .collectFrom([ref]);
+    const [, , mintTx] = await tx.chain();
+    txs.push(mintTx);
+
+    for (const tx of txs) console.log((await tx.complete()).toCBOR());
+  });
+
+const validate = <T, U>(validator: Type<T, U>, data: unknown) => {
+  const result = validator(data);
+  if (result instanceof type.errors) return logThenExit(result.summary);
+  return result;
+};
+
+const logThenExit = (message: string): never => {
+  console.error(message);
+  process.exit(1);
+};
+
+const createScript = (plutus: string, ref: UTxO): MintingPolicy => {
+  return {
+    type: "PlutusV2",
+    script: applyParamsToScript(plutus, [ref.txHash, BigInt(ref.outputIndex)]),
+  };
+};
+
+const TokenName = type("string<=20").pipe((s) => fromText(s));
+
+const Amount = type("string")
+  .pipe((s, ctx) => {
+    try {
+      return BigInt(s);
+    } catch {
+      return ctx.error("valid non-decimal number");
+    }
+  })
+  .narrow((v) => v > 0n);
+
+const Config = type({
+  BLOCKFROST_API_KEY: "string==39",
+});
+
+const Plutus = type({
+  validators: [
+    {
+      compiledCode: "string",
+    },
+  ],
+}).pipe((v) => v.validators[0].compiledCode);
+
+const convertNetwork = (blockfrost: CardanoBlockfrost) => {
+  const network = blockfrost.network;
+  type CardanoNetwork = typeof network;
+  const networks: Record<CardanoNetwork, Network> = {
+    mainnet: "Mainnet",
+    preprod: "Preprod",
+    preview: "Preview",
+  };
+  return networks[network];
+};
+
+program.parseAsync(process.argv);
