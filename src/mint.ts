fb0717a5bb6a049f8a9bd9931bbf08b823dd5b0a
SynthLuvr <131367121+SynthLuvr@users.noreply.github.com>
2024-07-03 12:52:36 +0200

    Reproduce lucid-evolution bug

--- src/mint.ts
+++ src/mint.ts
@@ -1,37 +1,17 @@
-import {
-  applyParamsToScript,
-  Data,
-  MintingPolicy,
-  mintingPolicyToId,
-  Network,
-  TxSignBuilder,
-  UTxO,
-  validatorToAddress,
-} from "@lucid-evolution/lucid";
+import { Data, MintingPolicy, mintingPolicyToId } from "@lucid-evolution/lucid";
 import { Command } from "commander";
 import { Problem } from "ts-handling";
 import { loadLucid } from "wallet";
-import {
-  Address,
-  Amount,
-  Config,
-  logThenExit,
-  TokenName,
-  validate,
-} from "./inputs";
+import { Address, Config, logThenExit, validate } from "./inputs";
 import { loadPlutus } from "./script";
-import { getNetwork, loadWallet } from "./wallet";
+import { loadWallet } from "./wallet";
 
 const program = new Command()
   .name("mint")
   .description("Mint a new token")
   .argument("<address>", "The address of the wallet performing the mint")
-  .argument("<name>", "The name of the token to mint")
-  .argument("<amount>", "The amount of token to mint")
-  .action(async ($address, $name, $amount) => {
+  .action(async ($address) => {
     const address = validate(Address, $address);
-    const name = validate(TokenName, $name);
-    const amount = validate(Amount, $amount);
     const config = validate(Config, process.env);
 
     const projectId = config.BLOCKFROST_API_KEY;
@@ -41,66 +21,23 @@ const program = new Command()
     const plutus = (await loadPlutus()).unwrap();
     if (plutus instanceof Problem) return logThenExit(plutus.error);
 
-    const network = getNetwork(projectId);
-    const txs: TxSignBuilder[] = [];
     const lucid = await loadLucid(projectId);
     lucid.selectWallet.fromAddress(wallet.address, wallet.utxos);
 
-    const setup = lucid
-      .newTx()
-      .pay.ToAddress(wallet.address, { lovelace: 2000000n });
-    const [[ref, ...setupUtxos], , setupTx] = await setup.chain();
-    lucid.selectWallet.fromAddress(wallet.address, setupUtxos);
-    txs.push(setupTx);
-
-    const blackhole = createBlackholeAddress(network);
-    const script = createScript(plutus, ref);
+    const script: MintingPolicy = {
+      type: "PlutusV2",
+      script: "51010000322253330034a229309b2b2b9a01",
+    };
     const policy = mintingPolicyToId(script);
-    const token = policy + name;
-    const deploy = lucid
-      .newTx()
-      .pay.ToContract(
-        blackhole,
-        { kind: "inline", value: Data.void() },
-        undefined,
-        script
-      );
-    const [deployUtxos, [refScript], deployTx] = await deploy.chain();
-    if (!refScript.scriptRef) return logThenExit("Script didn't deploy");
-    lucid.selectWallet.fromAddress(wallet.address, deployUtxos);
-    txs.push(deployTx);
+    const token = policy + "6578616d706c65";
 
     const tx = lucid
       .newTx()
-      .mintAssets({ [token]: amount }, Data.void())
-      .readFrom([refScript])
-      .collectFrom([ref]);
-    const [, , mintTx] = await tx.chain();
-    txs.push(mintTx);
+      .mintAssets({ [token]: 1n }, Data.void())
+      .attach.MintingPolicy(script);
 
-    for (const tx of txs) console.log((await tx.complete()).toCBOR());
-
-    console.log(`\nReference: ${refScript.txHash}`);
+    const completed = await (await tx.complete()).complete();
+    console.log(completed.toCBOR());
   });
-
-const createScript = (plutus: string, ref: UTxO): MintingPolicy => {
-  return {
-    type: "PlutusV2",
-    script: applyParamsToScript(plutus, [ref.txHash, BigInt(ref.outputIndex)]),
-  };
-};
-
-const createBlackholeAddress = (network: Network) => {
-  const header = "5839010000322253330033371e9101203";
-  const body = Array.from({ length: 63 }, () =>
-    Math.floor(Math.random() * 10)
-  ).join("");
-  const footer = "0048810014984d9595cd01";
-
-  return validatorToAddress(network, {
-    type: "PlutusV2",
-    script: `${header}${body}${footer}`,
-  });
-};
 
 program.parseAsync(process.argv);
